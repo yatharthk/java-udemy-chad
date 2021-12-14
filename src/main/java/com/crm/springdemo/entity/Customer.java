3d2ad8ec1db03163dcaec2d2cfa4c13cff959ba0
yatharthk <82939639+yatharthk@users.noreply.github.com>
2021-12-14 12:16:58 +0530

    Update Customer.java

--- src/main/java/com/crm/springdemo/entity/Customer.java
+++ src/main/java/com/crm/springdemo/entity/Customer.java
@@ -15,7 +15,7 @@ public class Customer {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
-	private String id;
+	private int id;
 	
 	@Column(name="first_name")
 	private String firstName;
@@ -35,11 +35,11 @@ public class Customer {
 		this.email = email;
 	}
 
-	public String getId() {
+	public int getId() {
 		return id;
 	}
 
-	public void setId(String id) {
+	public void setId(int id) {
 		this.id = id;
 	}
 
