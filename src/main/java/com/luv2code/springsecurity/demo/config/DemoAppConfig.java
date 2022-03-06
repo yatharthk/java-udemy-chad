3ec192155b9c9bda6fc11b5115c10e9f09d86162
yatharthk <82939639+yatharthk@users.noreply.github.com>
2022-03-06 17:42:32 +0530

    Update DemoAppConfig.java

--- src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
+++ src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
@@ -26,15 +26,7 @@ public class DemoAppConfig implements WebMvcConfigurer{
 		viewResolver.setSuffix(".jsp");
 		
 		return viewResolver;
-	}
-	
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/css/**").addResourceLocations("/WEB-INF/css/");
-	}
-	
-	
-	
+	}	
 	
 }
 
