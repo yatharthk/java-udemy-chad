c0317521a6f77a99b04a7787ae570d16ea783166
yatharthk <yatharthk8241@gmail.com>
2022-03-06 17:36:56 +0530

    customLoinFormWith:Optional external css

---
+++ src/main/java/com/luv2code/springsecurity/demo/security/DemoSecurityConfig.java
@@ -0,0 +1,42 @@
+package com.luv2code.springsecurity.demo.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		//add our users for in-memory database
+		
+		UserBuilder users =User.withDefaultPasswordEncoder();
+		
+		auth.inMemoryAuthentication()
+		.withUser(users.username("john").password("test123").roles("EMPLOYEE"))
+		.withUser(users.username("mary").password("test123").roles("ADMIN"))
+		.withUser(users.username("susan").password("test123").roles("MANAGER"));	
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.antMatchers("/css/**").permitAll()
+		.anyRequest().authenticated()
+		.and()
+		.formLogin()
+			.loginPage("/showMyLoginPage")
+			.loginProcessingUrl("/authenticateTheUser")
+			.permitAll();
+		
+	}
+	
+	
+
+}
---
+++ src/main/webapp/WEB-INF/view/plain-login.jsp
@@ -0,0 +1,38 @@
+<%@page import="javax.servlet.descriptor.TaglibDescriptor"%>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<html>
+<style ></style>
+<head>
+<meta charset="ISO-8859-1">
+<title>Custom Login Page</title>
+<link rel="stylesheet" type="text/css" href="css/demo.css">
+</head>
+<body>
+	<h3>My Custom Login Page</h3>
+
+	<form:form
+		action="${pageContext.request.contextPath}/authenticateTheUser"
+		method="POST">
+		
+		<c:if test="${param.error != null}">
+			<i class="flagged">Sorry! You entered Invalid user-name/password</i>
+		</c:if>
+		
+		<p>
+			User name : <input type="text" name="username" />
+		</p>
+
+
+		<p>
+			Password : <input type="password" name="password" />
+		</p>
+
+		<input type="submit" value="Login" />
+
+	</form:form>
+</body>
+</html>
