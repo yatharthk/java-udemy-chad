c677fdcd708b7fd594e0358743e8a323dc488487
yatharthk <yatharthk8241@gmail.com>
2022-03-06 17:45:39 +0530

    CustomLoginFormBootstrapped

---
+++ src/main/java/com/luv2code/springsecurity/demo/security/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package com.luv2code.springsecurity.demo.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+	//No code required here.
+}
