ca306697a687650fd90a7e54bbe9e7b4f68ed8d7
yatharthk <yatharthk8241@gmail.com>
2022-05-08 01:29:16 +0530

    Using basic Hibernate crud

---
+++ src/main/java/com/example/demo/entity/Employee.java
@@ -0,0 +1,29 @@
+package com.example.demo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+}
