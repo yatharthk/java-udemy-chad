bae84f75cb30c61eb2df77ad7fa90d1287101cec
yatharthk <yatharthk8241@gmail.com>
2022-05-09 00:57:29 +0530

    thymeleaf-demo-bootstrap-getList+getForm+addForm

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,44 +1,34 @@
 package com.example.demo.controller;
 
-import com.example.demo.model.Employee;
+import com.example.demo.entity.Employee;
+import com.example.demo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
-    List<Employee> employees;
 
-    @PostConstruct
-    private void loadData(){
-
-
-        Employee employee = new Employee(1,"Leslie","Andrews","leslie@gmail.com");
-        Employee employee2 = new Employee(2,"Ashley","Andrews","ashlie@gmail.com");
-        Employee employee3 = new Employee(3,"Avni","Gupta","avni@gmail.com");
-        employees = new ArrayList<>();
-        employees.add(employee);
-        employees.add(employee2);
-        employees.add(employee3);
-    }
+    @Autowired
+    private EmployeeService employeeService;
 
     @GetMapping("/list")
     public String getAllEmployees(Model model) {
-        Employee employee = new Employee(1,"Leslie","Andrews","leslie@gmail.com");
-        Employee employee2 = new Employee(2,"Ashley","Andrews","ashlie@gmail.com");
-        Employee employee3 = new Employee(3,"Avni","Gupta","avni@gmail.com");
+        List<Employee> employeeList = employeeService.getAllEmployees();
+        model.addAttribute("employees",employeeList);
+        return "employees/list-employees";
+    }
 
-
-
-        model.addAttribute("employees",employees);
-
-        return "list-employees";
-
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model model){
+        Employee employee = new Employee();
+        model.addAttribute("employee",employee);
+        return "employees/employee-form";
     }
 }
---
+++ src/main/java/com/example/demo/entity/Employee.java
@@ -0,0 +1,31 @@
+package com.example.demo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+
+}
