cd3e033285a2315d5e968526ccf22aa2fb6201fe
yatharthk <yatharthk8241@gmail.com>
2022-05-09 00:57:47 +0530

    thymeleaf-demo-bootstrap-getList+getForm+addForm

---
+++ src/main/java/com/example/demo/service/EmployeeServiceImpl.java
@@ -0,0 +1,21 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Employee;
+import com.example.demo.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+}
+
