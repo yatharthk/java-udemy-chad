aa0e3c55022ba0d46d99ef02a592d53b7f20e679
yatharthk <yatharthk8241@gmail.com>
2022-05-08 17:59:23 +0530

    SpringBoot+PAGINTION&SORTING (COMES AS DEFAULTS WITH DATA-REST)

--- src/main/java/com/example/demo/dao/EmployeeRepository.java
+++ src/main/java/com/example/demo/dao/EmployeeRepository.java
@@ -2,8 +2,9 @@ package com.example.demo.dao;
 
 import com.example.demo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@Repository
+//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
